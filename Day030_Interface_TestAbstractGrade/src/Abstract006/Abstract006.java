2ea56ae87d0ffbb2fa795ec8682e3423ae178267
youjininreallife <gracecancho@gmail.com>
2020-11-05 22:43:30 +0900

    Revert "Revert "Revert "Abstract, Interface_intro"""
    This reverts commit 47c8b118c3db2023b3c973d0dbeeeb2bb76e7eda.

--- Day030_Interface_TestAbstractGrade/src/Abstract006/Abstract006.java
+++
@@ -1,9 +0,0 @@
-package Abstract006;
-
-public class Abstract006 {
-	public static void main(String[] args) {
-		ScoreUsing std = new ScoreUsing("헐크",100,100,99);
-		std.exec1();
-		std.exec2();
-	}
-}
